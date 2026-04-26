085a7b05bd40396afc88279db1ecc28f0ad090c4
Anilchaudhary2011 <anilpatel2011978@gmail.com>
2026-04-26 23:30:41 +0530

    update test case

--- src/tests/api.test.js
+++ src/tests/api.test.js
@@ -1,10 +1,35 @@
 import request from "supertest";
 import app from "../index.js";
 
-describe("API Test", () => {
-  it("should return 200 from /api", async () => {
-    const res = await request(app).get("/api");
+// describe("Protected API Test", () => {
 
-    expect(res.statusCode).toBe(200);
+//   let token;
+
+//   beforeAll(async () => {
+//     const res = await request(app)
+//       .post("/auth/userlogin")
+//       .send({
+//         email: "anilchaudhary2011@gmail.com",
+//         password: "Anil@125"
+//       });
+
+//     token = res.body.token;
+//   });
+
+//   it("should return 200 from /api/user", async () => {
+//     const res = await request(app)
+//       .get("/api/user")
+//       .set("Authorization", `Bearer ${token}`);
+
+//     expect(res.statusCode).toBe(200);
+//   });
+
+// });
+
+describe("API Test", () => {
+  it("should return 200 from /api/user", async () => {
+    const res = await request(app).get("/api/user");
+
+    expect(res.statusCode).toBe(401);
   });
 });
